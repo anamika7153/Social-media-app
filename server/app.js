a08cc45edf24de655ef85ba52e9ac316ace8b8c7
Anamika Yadav <aanyayadav419@gmail.com>
2023-08-26 15:01:05 +0530

    directory struct

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ app.use( '/api/users', usersRoutes);
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
