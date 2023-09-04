ba5758facbd2720e02521c3a44848fdabff91834
Anamika Yadav <aanyayadav419@gmail.com>
2023-09-04 23:25:08 +0530

    minor

--- fusionflock/src/pages/Home.jsx
+++ fusionflock/src/pages/Home.jsx
@@ -1,11 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
-import React, { useContext, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import firebase from "firebase/app";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-
-const storage = getStorage();
 
 const Home = () => {
   
@@ -26,22 +21,7 @@ const Home = () => {
         <input type="file" name="image" accept="image/*" required />
         <button type="submit">Create Post</button>
       </form>
-      Home
-      <form
-        action="/api/users/posts"
-        method="POST"
-        enctype="multipart/form-data"
-      >
-        <input type="text" name="caption" placeholder="caption" required />
-        {/* <textarea name="content" placeholder="Content" required></textarea> */}
-        <input type="file" name="image" accept="image/*" required />
-        <button type="submit">Create Post</button>
-      </form>
     </div>
   );
 };
-  );
-};
-
-export default Home;
 export default Home;
